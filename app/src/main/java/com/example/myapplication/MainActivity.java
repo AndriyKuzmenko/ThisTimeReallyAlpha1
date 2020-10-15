ea18930615a1e4e11c23784c10f06ca011846526
Andriy <ak8022@bs.amalnet.k12.il>
2020-10-15 11:25:23 +0300

    Removed countries with less than 5 cities and replaced the with countries that have more than 5 cities.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,10 +9,10 @@ public class MainActivity extends AppCompatActivity
     String[] continents={"-Continent-","Africa","Asia","Europe","N. America", "Oceania","S. America"}; //N.=North    S.=South
 
     String[][] countries={{"Angola","Botswana","Cameroon","Egypt","Eritrea","Liberia","Senegal","S. Africa"},
-                          {"Hong Kong","Israel","Japan","Nepal","Singapore","Sri Lanka","S. Korea","Taiwan"},
+                          {"India","Israel","Japan","Nepal","Philippines","Sri Lanka","S. Korea","Taiwan"},
                           {"Albania","Belgium","Finland","France","Germany","Norway","Spain","United Kingdom"},
-                          {"Anguila","Belize","Bermuda","Canada","Cayman Islands","Greenland","St. Martin","United States"},
-                          {"Australia","French Polynesia","Guam","Marshall Islands","New Caledonia","New Zealand","Pitcairn Islands","Samoa"},
+                          {"Anguila","Bahamas","Belize","Canada","Cayman Islands","Greenland","St. Martin","United States"},
+                          {"Australia","Cook Islands","French Polynesia","Guam","New Caledonia","New Zealand","Niue","Samoa"},
                           {"Argentina","Bolivia","Brazil","Chile","Falkland Islands","French Guiana","Peru","Uruguay"}};
 
     @Override
