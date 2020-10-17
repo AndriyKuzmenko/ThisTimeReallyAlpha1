f62eb3dd08da8638c3678d0b460d4b394a31e960
Andriy <ak8022@bs.amalnet.k12.il>
2020-10-17 18:40:42 +0300

    Fixed spinners

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,14 +12,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 {
     String[] continents={"-Continent-","Africa","Asia","Europe","N. America", "Oceania","S. America"}; //N.=North    S.=South
 
-    String[][] countries={{"Angola","Botswana","Cameroon","Egypt","Eritrea","Liberia","Senegal","S. Africa"},
-                          {"India","Israel","Japan","Nepal","Philippines","Sri Lanka","S. Korea","Taiwan"},
-                          {"Albania","Belgium","Finland","France","Germany","Norway","Spain","United Kingdom"},
-                          {"Anguila","Bahamas","Belize","Canada","Cayman Islands","Greenland","St. Martin","United States"},
-                          {"Australia","Cook Islands","French Polynesia","Guam","New Caledonia","New Zealand","Niue","Samoa"},
-                          {"Argentina","Bolivia","Brazil","Chile","Falkland Islands","French Guiana","Peru","Uruguay"}};
+    String[][] countries={{"-Country-","Angola","Botswana","Cameroon","Egypt","Eritrea","Liberia","Senegal","S. Africa"},
+                          {"-Country-","India","Israel","Japan","Nepal","Philippines","Sri Lanka","S. Korea","Taiwan"},
+                          {"-Country-","Albania","Belgium","Finland","France","Germany","Norway","Spain","United Kingdom"},
+                          {"-Country-","Anguila","Bahamas","Belize","Canada","Cayman Islands","Greenland","St. Martin","United States"},
+                          {"-Country-","Australia","Cook Islands","French Polynesia","Guam","New Caledonia","New Zealand","Niue","Samoa"},
+                          {"-Country-","Argentina","Bolivia","Brazil","Chile","Falkland Islands","French Guiana","Peru","Uruguay"}};
 
     Spinner continentsSpinner, countriesSpinner;
+    int continent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,6 +29,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
 
         continentsSpinner=(Spinner)findViewById(R.id.continentSpinner);
+        countriesSpinner=(Spinner)findViewById(R.id.countriesSpinner);
+
+        continentsSpinner.setOnItemSelectedListener(this);
 
         ArrayAdapter<String> adp=new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,continents);
         continentsSpinner.setAdapter(adp);
@@ -42,7 +46,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
     {
-
+        if (parent.getId()==continentsSpinner.getId())
+        {
+            if (position == 0) return;
+            ArrayAdapter<String> adp = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, countries[position - 1]);
+            countriesSpinner.setAdapter(adp);
+            continent=position-1;
+        }
     }
 
     @Override
